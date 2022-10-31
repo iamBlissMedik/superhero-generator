5a833ae203ff62c391d87324a2b96f647306bb71
Ellams Bliss <ellamsbliss@gmail.com>
2022-10-31 21:57:17 +1100

    show the superhero image

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const heroName = document.querySelector(".name");
 const inputValue = document.querySelector("#input-value");
 const randomGen = document.querySelector("#random-gen");
+const superHeroImage = document.querySelector("#superhero-image");
 const superHeroAPI = "https://www.superheroapi.com/api.php/5742301652531542";
 const loadAllEventListeners = () => {
   randomGen.addEventListener("click", getSuperHero);
@@ -11,7 +12,10 @@ const getSuperHero = () => {
   fetch(`${superHeroAPI}/${randomSuperHero()}`)
     .then((response) => response.json())
     .then((json) => {
-      heroName.textContent = `${json.name}`;
+        heroName.textContent = `${json.name}`;
+        superHeroImage.src = `${json.image.url}`
+        console.log(json);
+        
     });
 };
 
