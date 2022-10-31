a1837a920d97899dae68d890bebb522f8bc3d533
Ellams Bliss <ellamsbliss@gmail.com>
2022-10-31 21:33:48 +1100

    auto generate superhero name

---
+++ app.js
@@ -0,0 +1,27 @@
+const heroName = document.querySelector(".name");
+const inputValue = document.querySelector("#input-value");
+const randomGen = document.querySelector("#random-gen");
+const superHeroAPI = "https://www.superheroapi.com/api.php/5742301652531542";
+const loadAllEventListeners = () => {
+    randomGen.addEventListener("click", getSuperHero);
+}
+
+// generate button
+const getSuperHero = () => {
+  fetch(`${superHeroAPI}/${randomSuperHero()}`)
+    .then((response) => response.json())
+    .then((json) => {
+      heroName.textContent = `${json.name}`;
+    });
+   
+    
+  
+};
+
+// randomly generate superhero
+const randomSuperHero = () => {
+  return Math.floor(Math.random() * 732 + 1);
+};
+
+loadAllEventListeners()
+
