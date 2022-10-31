59ae6cbbf856688c48dc7acfe9d755f3ca32b239
Ellams Bliss <ellamsbliss@gmail.com>
2022-10-31 22:38:11 +1100

    added search feature.where you can search for a superhero,and the name will pop up

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 const heroName = document.querySelector(".name");
-const inputValue = document.querySelector("#input-value");
+
 const randomGen = document.querySelector("#random-gen");
 const superHeroImage = document.querySelector("#superhero-image");
+const search = document.querySelector("#search");
 const superHeroAPI = "https://www.superheroapi.com/api.php/5742301652531542";
 const loadAllEventListeners = () => {
-  randomGen.addEventListener("click", getSuperHero);
+    randomGen.addEventListener("click", getSuperHero);
+    search.addEventListener("click", searchSuperHero);
 };
 
-// generate button
+// auto generate button
 const getSuperHero = () => {
   fetch(`${superHeroAPI}/${randomSuperHero()}`)
     .then((response) => response.json())
     .then((json) => {
-        heroName.textContent = `${json.name}`;
-        superHeroImage.src = `${json.image.url}`
-        console.log(json);
-        
+      heroName.textContent = `${json.name}`;
+      superHeroImage.src = `${json.image.url}`;
     });
 };
 
-// randomly generate superhero
+// randomly generate superhero function
 const randomSuperHero = () => {
   const totalSuperHero = 732;
   return Math.floor(Math.random() * totalSuperHero + 1);
+};
+
+// search for superhero
+const searchSuperHero = () => {
+const inputValue = document.querySelector("#input-value").value;
+  fetch(`${superHeroAPI}/search/${inputValue}`)
+    .then((response) => response.json())
+      .then((json) => {
+          superHeroImage.src = `${json.results[0].image.url}`;
+          heroName.textContent = `${json.results[0].name}`;
+      
+    });
 };
 
 loadAllEventListeners();
