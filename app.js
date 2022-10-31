06401a2f2f1ff7f447b50f76dcd9f636cebd4265
Ellams Bliss <ellamsbliss@gmail.com>
2022-11-01 01:27:41 +1100

    search for superhero and show it to the dom

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const superHeroImage = document.querySelector("#superhero-image");
 const search = document.querySelector("#search");
 const superHeroAPI = "https://www.superheroapi.com/api.php/5742301652531542";
 const loadAllEventListeners = () => {
-    randomGen.addEventListener("click", getSuperHero);
-    search.addEventListener("click", searchSuperHero);
+  randomGen.addEventListener("click", getSuperHero);
+  search.addEventListener("click", searchSuperHero);
 };
 
 // auto generate button
@@ -14,7 +14,7 @@ const getSuperHero = () => {
   fetch(`${superHeroAPI}/${randomSuperHero()}`)
     .then((response) => response.json())
     .then((json) => {
-     superheroDetails(json)
+      superheroDetails(json);
     });
 };
 
@@ -26,21 +26,32 @@ const randomSuperHero = () => {
 
 // search for superhero
 const searchSuperHero = () => {
-const inputValue = document.querySelector("#input-value").value;
+  const inputValue = document.querySelector("#input-value").value;
   fetch(`${superHeroAPI}/search/${inputValue}`)
     .then((response) => response.json())
-      .then((json) => {
-          const theHero = json.results[0]
-          superheroDetails(theHero);
-      
+    .then((json) => {
+      const theHero = json.results[0];
+
+      superheroDetails(theHero);
     });
 };
 
+// superhero full details and appending it to the dom
 const superheroDetails = (superhero) => {
-    let superheroName = superhero.name
-    let searchSuperHeroImgUrl = superhero.image.url;
-     heroName.textContent = `${superheroName}`;
-     superHeroImage.src = `${searchSuperHeroImgUrl}`;
-}
+  let superheroName = superhero.name;
+  let searchSuperHeroImgUrl = superhero.image.url;
+  const powerStats = document.querySelector(".power-stats");
+
+  heroName.textContent = `${superheroName}`;
+  superHeroImage.src = `${searchSuperHeroImgUrl}`;
+
+  let stats = Object.keys(superhero.powerstats)
+    .map((ability) => {
+      return `<p>${ability} : ${superhero.powerstats[ability]}</p>`;
+    })
+    .join(" ");
+
+  powerStats.innerHTML = stats;
+};
 
 loadAllEventListeners();
